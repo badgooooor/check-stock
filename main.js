f6ccba846abecdd465a36d4d73cf52ec2d8b0c91
_borBeer <60010842@kmitl.ac.th>
2019-01-12 15:17:19 +0700

    🗃 Add initialize and create database

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
+const model = require('./src/db/model');
 
 const {app, BrowserWindow, Menu, ipcMain} = electron;
 let mainwindow;
@@ -14,19 +15,26 @@ const createWindow = () => {
         minWidth: 1024,
         minHeight: 768
     });
-    mainwindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'browser', 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
+
+    model.initDb(app.getPath('userData'), 
+        mainwindow.loadURL(url.format({
+            pathname: path.join(__dirname, 'browser', 'index.html'),
+            protocol: 'file:',
+            slashes: true
+        }))
+    );
 
     mainwindow.on('closed', () => {
         app.quit();
     });
 
     require('./src/renderer/mainMenu');
+
+    mainwindow.on('closed', () => mainwindow = null);
 };
 
 
 
+
 app.on('ready', createWindow);
+app.on('window-all-closed', () => app.quit());
--- src/db/model.js
+++ src/db/model.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const path = require('path')
 const fs = require('fs')
 const SQL = require('sql.js')
@@ -8,58 +10,100 @@ const SQL = require('sql.js')
   values into a single objects and collects these together by row id.
 */
 let _rowsFromSqlDataObject = function (object) {
-    let data = {};
-    let i = 0;
-    let j = 0;
+    let data = {}
+    let i = 0
+    let j = 0
     for (let valueArray of object.values) {
-        data[i] = {};
-        j = 0;
-        for (let column of object.columns) {
-            Object.assign(data[i], {[column]: valueArray[j]});
-            j++;
-        }
-        i++;
+      data[i] = {}
+      j = 0
+      for (let column of object.columns) {
+        Object.assign(data[i], {[column]: valueArray[j]})
+        j++
+      }
+      i++
     }
-    return data;
-};
+    return data
+}
 
 // Return a string of placeholders for use in a prepared statement.
 let _placeHoldersString = function (length) {
-    let places = '';
+    let places = ''
     for (let i = 1; i <= length; i++) {
-        places += '?, ';
+      places += '?, '
     }
-    return /(.*),/.exec(places)[1];
+    return /(.*),/.exec(places)[1]
 }
 
 // Open and close connection handler.
 SQL.dbOpen = function (databaseFileName) {
     try {
-        return new SQL.Database(fs.readFileSync(databaseFileName));
+      return new SQL.Database(fs.readFileSync(databaseFileName))
     } catch (error) {
-        console.log("Can't open database file.", error.message);
-        return null;
+      console.log("Can't open database file.", error.message)
+      return null
     }
-};
-
+}
+  
 SQL.dbClose = function (databaseHandle, databaseFileName) {
     try {
-        let data = databaseHandle.export();
-        let buffer = Buffer.alloc(data.length, data);
-        fs.writeFileSync(databaseFileName, buffer);
-        databaseHandle.close();
-        return true;
+      let data = databaseHandle.export()
+      let buffer = Buffer.alloc(data.length, data)
+      fs.writeFileSync(databaseFileName, buffer)
+      databaseHandle.close()
+      return true
     } catch (error) {
-        console.log("Can't close database file.", error.message);
-        return null;
+      console.log("Can't close database file.", error)
+      return null
     }
-};
+}
 
 // Initialize a new SQLite3 database from schema.sql
 // (Called from main.js during initialization)
 module.exports.initDb = function (appPath, callback) {
-
-};
+    let dbPath = path.join(appPath, 'example.db')
+    let createDb = function (dbPath) {
+      // Create a database.
+      let db = new SQL.Database()
+      let query = fs.readFileSync(
+      path.join(__dirname, 'schema.sql'), 'utf8')
+      let result = db.exec(query)
+      if (Object.keys(result).length === 0 &&
+        typeof result.constructor === 'function' &&
+        SQL.dbClose(db, dbPath)) {
+        console.log('Created a new database.')
+      } else {
+        console.log('model.initDb.createDb failed.')
+      }
+    }
+    let db = null;
+    try {
+        db = SQL.dbOpen(dbPath)
+    } catch (err) {
+        createDb(dbPath)
+        console.log(err)
+    }
+    if (db === null) {
+      /* The file doesn't exist so create a new database. */
+      createDb(dbPath)
+    } else {
+      /*
+        The file is a valid sqlite3 database. This simple query will demonstrate
+        whether it's in good health or not.
+      */
+      let query = 'SELECT count(*) as `count` FROM `sqlite_master`'
+      let row = db.exec(query)
+      let tableCount = parseInt(row[0].values)
+      if (tableCount === 0) {
+        console.log('The file is an empty SQLite3 database.')
+        createDb(dbPath)
+      } else {
+        console.log('The database has', tableCount, 'tables.')
+      }
+      if (typeof callback === 'function') {
+        callback()
+      }
+    }
+  }
 
 // List products.
 module.exports.getProducts = function () {
