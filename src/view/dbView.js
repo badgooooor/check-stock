cbc19508ba7d4119a42f37e89e3d706121cfdf09
_borBeer <60010842@kmitl.ac.th>
2019-01-24 14:28:16 +0700

    :fire: Remove useless codes

--- src/view/dbView.js
+++ src/view/dbView.js
@@ -5,11 +5,11 @@ module.exports.showStock = function (rowsObject) {
     let markup = ''
     for (let rowId in rowsObject) {
         let row = rowsObject[rowId]
-        console.log(row)
         markup += '<tr>'
         + '<td>' + row.id + '</td>'
         + '<td>' + row.name + '</td>'
         + '</tr>'
     }
-    return markup
+    const list = document.getElementById("stock-list")
+    list.insertAdjacentHTML('beforeend', markup)
 }
